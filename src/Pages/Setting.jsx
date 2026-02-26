709765110546436283144476bd48e9aeef981ac7
sumonkhan0077 <sumonkhanbd0077@gmail.com>
2026-02-26 12:18:56 +0600

    added mainlayout create

---
+++ src/Pages/Setting.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Setting = () => {
+    return (
+        <div>
+            this Setting
+        </div>
+    );
+};
+
+export default Setting;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,16 +1,20 @@
 import { createBrowserRouter } from "react-router";
+import MainLayout from "../Layouts/MainLayout";
+import Setting from "../Pages/Setting";
+import Dashboard from "../Pages/Dashboard";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    element:<MainLayout/>,
     children: [
       {
         index: true,
-        element: "",
+        element:<Dashboard/>,
       },
       {
-        path: "home",
-        element: "",
+        path: "setting",
+        element: <Setting/>,
       },
     ],
   },
